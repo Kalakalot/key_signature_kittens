8ba5d7bdde671ef2192fe14d7d0ba6724bdda159
Julia A Kingrey <juliakingreyworks@gmail.com>
2020-01-10 15:18:10 -0800

    Added prop types to Question.js

--- src/components/Question.js
+++ src/components/Question.js
@@ -3,13 +3,10 @@ import PropTypes from 'prop-types';
 
   const Question = (props) => {
     
-    console.log(props)
-
     return (
       <div>
         <h2 className="question">What key signature is this?</h2>
         <img src={props.trebleURL} alt={props.alt}/>
-        <p>{props.answerOptions}</p>
       </div>
     );
   }
@@ -17,6 +14,6 @@ import PropTypes from 'prop-types';
   export default Question;
 
   Question.propTypes = {
-    // treble_url: PropTypes.string.isRequired,
-    // bass_url: PropTypes.string.isRequired
+    treble_url: PropTypes.string.isRequired,
+    alt: PropTypes.string.isRequired
   };
