8828d6fa939ba904494f825ee5b5c138e2bc3c65
Julia A Kingrey <juliakingreyworks@gmail.com>
2020-01-13 12:34:19 -0800

    Added Quiz component that currently holds Question data. Next step: add AnswerOptions component to Quiz.

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,33 +1,39 @@
 import React from 'react'; 
 import Question from './Question'; 
-import QuestionCount from './QuestionCount'; 
+// import QuestionCount from './QuestionCount'; 
 import AnswerOption from './AnswerOption';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
-function Quiz(props) {
+const Quiz = (props) => {
   return (
     <div className="quiz">
-      <QuestionCount
+      {/* <QuestionCount
         counter={props.questionId}
         total={props.questionTotal}
-      />
-      <Question content={props.question} />
+      /> */}
+      <Question
+          // key={i}
+          // keySignature={props.key_signature}
+          // trebleURL={props.treble_url}
+          // alt={props.alt}
+          // answerOptions={question["answer_options"]}
+          // answerExplanation={question["answer_explanation"]}
+          />
       <ul className="answerOptions">
-        {props.answerOptions.map(renderAnswerOptions)}
+        {/* {props.answerOptions.map(renderAnswerOptions)} */}
       </ul>
     </div>
   );
 }
 
-
 export default Quiz;
 
 Quiz.propTypes = {
-  answer: PropTypes.string.isRequired,
-  answerOptions: PropTypes.array.isRequired,
-  counter: PropTypes.number.isRequired,
-  question: PropTypes.string.isRequired,
-  questionId: PropTypes.number.isRequired,
-  questionTotal: PropTypes.number.isRequired,
-  onAnswerSelected: PropTypes.func.isRequired
+  // answer: PropTypes.string.isRequired,
+  // answerOptions: PropTypes.array.isRequired,
+  // counter: PropTypes.number.isRequired,
+  // question: PropTypes.string.isRequired,
+  // questionId: PropTypes.number.isRequired,
+  // questionTotal: PropTypes.number.isRequired,
+  // onAnswerSelected: PropTypes.func.isRequired
 };
