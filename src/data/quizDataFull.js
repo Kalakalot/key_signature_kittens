2cc78fd3f5f58cce50bd4f2d6949af287d41a7b5
Julia A Kingrey <juliakingreyworks@gmail.com>
2020-01-16 13:44:51 -0800

    Started building out full quiz data set. Need to add key signature images and explanation images.

---
+++ src/data/quizDataFull.js
@@ -0,0 +1,526 @@
+const quizData = 
+[
+  {
+    "_id": {
+      "$oid": "5e13b1d21c9d440000086025"
+    },
+    "answers": [
+      {
+        "content": "D major",
+        "type": "incorrect"
+      },
+      {
+        "content": "G major",
+        "type": "incorrect"
+      },
+      {
+        "content": "A major",
+        "type": "incorrect"
+      },
+      {
+        "content": "C major",
+        "type": "correct"
+      },
+    ],
+    "key_signature": "C major",
+    "kitten_value": "100",
+    "accidentals": [
+      ""
+    ],
+    "answer_explanation": "C major is the only major key with no accidentals. You \"C\" no sharps or flats here!",
+    "explanation_url": "https://placebear.com/300/300",
+    "question": "https://lh3.googleusercontent.com/bwDmcgD4YDYzKJstp1qYHFDWsqtR4K2SN9clK5B-Mc7v3zM6oXvAviCA7AODILxfwgQ9be2z6ZY9nwetu0SteBSyrBN44PXE6PcVyiLI4Jtl9wXMbJJjhNApjCYO_fPbcu7ElPJWv-ca1fcOnmH6sdjjlPklb1OhYPQdTEEmQWJtjbUXnJip0hmHs0rTEUw_Hwe7TY15Evz5SzAebSlmioLuBICEG_btcpWIT-_hYKy_TyoMfTiMCmxJi8yT5td-WIb_Bwx-0LIymGUMoJXKsbNm6g4Tt40g9hejmso4bS0NOVPaWtF4DF5evXIBbWUllrlhIhkEg0eRplVgMdtsfUi4NLeLSuPrk9tznHBILaFMA5MB8CJDsTKtKqNBhWCQ1Fg8N0ggw3yxsNP5K5ujyMOgnN2F0j4YAn9QdmdZ0ZKFWVL2j0J2P6bexg0JWciE9uDnwer5BcmiUqvtH0Q6htSB3oNx_ZDDN0gGjY7zrOm5fCkWQZKANq5v6Q9DBL9u1ZxdWQbvwI3bGd60pn6spgsjryX4ejAGKXtVwoOaCGvGbxqmrUnfZFBi_GxZCMZI2FGUcZ4Ntiz6p0jJrRhSUdxndB31dTKIMI2lriX78Wf9GVG5fhbOrilNMKw5I2PkpeahJjPDqpY6ZJuLEtGXlnFH6w8wMkYqBcrAQVUXZZ_X-Y-QQOwT0w=w298-h436-no",
+    "alt": "Key signature with no sharps or flats"
+  },
+  {
+    "_id": {
+      "$oid": "5e13b29b1c9d440000086026"
+    },
+    "answers": [
+      {
+        "content": "F major",
+        "type": "incorrect"
+      },
+      {
+        "content": "C major",
+        "type": "incorrect"
+      },
+      {
+        "content": "G major",
+        "type": "incorrect"
+      },
+      {
+        "content": "D major",
+        "type": "correct"
+      },
+    ],
+    "key_signature": "D major",
+    "kitten_value": "140",
+    "accidentals": [
+      "F sharp",
+      "C sharp"
+    ],
+    "answer_explanation": "You can determine the key signature of a major key with sharps by going a half step up from the last sharp. A half step up from C sharp is D, which makes this D major.",
+    "explanation_url": "https://lh3.googleusercontent.com/SofovhF-UzRgVWvxx1jko-sQ39nZkChMmRp4kri5Ds-fRCFyaK8xRJDMMV61lezuAV93r69V1K8MfN3LqKNJhVHBhRc2QI-TyVN1Ryyx7-q7OaTsTJDMth-V_YhwYnL6sT5QRlpRtafc3212-h44B7pkmNzkZa0mOCk1C1vsBUaxzCate0wi2E3vSR9XGEWWoFFfGhaZrHStKK8wF-COCoCBZk9NIuiUkV5KLse_vZlDTq_JZXCyX9kgOH8-zMK_FmZVlqR7SoPDezcqCDK8UiZERGqxc2o0seWTAasXUg33S6F6S3EnHJBSkReeDTK0T13mCzKSpDdztCEdY3E43yGDt7T-z7tQiEJqnzKPGhvcxHYxraWjbss7CZtyQoV4dsCkHtDq2YmTLSGSh26qOWQuJ2VwNfutkAOFJvz5sBT1bjI8SFy7GWXM8SwX4RaVObCNd51MD2ZiKoRhI-Tmti9msA1QGNSkq9mVncH5Aly4yTmPWzw5lgboYktAk-t5INLpX55QctqKaLNI_NXx4Fpw0Gj6pBhVpkZ57cekFAUWk3fx8zT_B_wDKbAUiE936QSEYAF_LQQkG1tyyUtHzvn72MDFAigg2yOxb6vPyocnwb6mxHTC9dR0yhzFk6yMd6ulu3O7QrpRIQQl290c9nZ98_sNhZo9pBgYs-RHTCuWlP39NWlTGA=w394-h436-no",
+    "question": "https://lh3.googleusercontent.com/2r90D7dnV-SvpyySjKTSzZsstTcaoWtE61wreACjnWkHT6kXfV3p3qsd-NqNzV-VwXSoD8z2MKn_zoeyHAgmfVNL3mc2rQI8y0IVHNmzucDUPpvYL74vArZG5iSj3Iil20oGLOnKk-ydPdrF9wIoNgVdLLK0ixhfJlYcYl_ymIKzzpz3PZPMSWaOakcl03BuSSPqs34w9Kkjn_HiKNMOB0kMs_SEUTT14S_lwo8QXf2TBuuUG6VCA0s7sUjxxolaTooY9bWYw70PLWH2canm_pXoHbY-zAuiN7MZfJrKA9Nwua_FW2yyif9csrLwZ8DCraxPxyDDQTucGWtIjJFIAmpaXrr-DmJSUdr5oPkNw07Fd0Ef9O4RDWlQKzuHZjhBm2O2Fw3-q3zTJbp2uwygbgdbYzYAMjYmpczP3jFx75gi_z3qx7npbGsnB7xxD53qJKshwsMopo1-tPwNfaw8dtxnTUFMZ6X-lnP7uibJ_ZTUcmZerzvVimkZKhNRjeP9NwN8g1IbKLXXyjBUZqnxGX-LmZ8jZQJPVt4kNXcQNIBZxRfsLLAC_NCKZy9NKYaxvjl3TrvsmuLo7UsxwA-gtWRymy8M3ZAC9r5H9Kj1v-VNtyG_CIvJ88ZLeHhmlEung3FlHRSaRbDbqFMICU3rYh_rewStjaRpI_DzbV35-pZ2BLeIUcnArg=w394-h436-no",
+    "alt": "Key signature with two sharps: F sharp and C sharp"
+  },
+  {
+    "_id": {
+      "$oid": "5e17b2b71c9d440000cc71c9"
+    },
+    "answers": [
+      {
+        "content": "F major",
+        "type": "incorrect"
+      },
+      {
+        "content": "A major",
+        "type": "incorrect"
+      },
+      {
+        "content": "D major",
+        "type": "incorrect"
+      },
+      {
+        "content": "G major",
+        "type": "correct"
+      },
+    ],
+    "key_signature": "G major",
+    "kitten_value": "120",
+    "accidentals": "F sharp",
+    "answer_explanation": "You can determine the key signature of a major key with sharps by going a half step up from the last sharp. A half step up from F sharp is G, which makes this G major.",
+    "explanation_url": "https://placebear.com/300/300",
+    "question": "https://lh3.googleusercontent.com/B6KCZzCsuM9TsSmTP_CCrv3TdHHqCzaDtHDff_fZRLfyN_rh_b9Pb6AlVL8wguf9Bputks0w9JB41k3K0Q-eGwWg5aWz-dXnJaCVQYoPfUXmxAh5qdRvGKF6t380jM9PEo62J5JoowpnskAyZv0WCV8Sr1j3ZBsmo2Kgqw9ix3iW_Q8ruYEvtW50lggNYtBAjuxZ8_zVlgsQj9mwMbIdI8rkV0iSyVNBklTT33DieiWKVMX5GKZxjevMCTCzm8zpgwFgjKhmaWmW7BT0vNaAvVSolvCP-LV0RNOdLBesvuzi-wYHEioLpJU2Tc3Lraz6D0jq8vykJIDNtKpSHrKGeEaDglWW40IzQfXJfDkMsQfYqE6j73Fy5uhneKthZX3L0YPUBHyYD0srNLn9wSioUieI_JuftVdNjdO9NF-gur4Lv0z9pFX84IddSzbTX54PTq0HPql9IuWz7QL-tP_gzKrX1TJIyA02bytHC5Rv2Df6up3qUqy4dJ3M5DT19aOB41I26kIjeD5Z1HGa5mLybsIvb3NvKi3qwSx917rlo-hN7SMqo8GjISWwUACw7xCIB9ojlqrvFVoZJs15u-XbUb4yHWDININZS_ryPtH8ZmFo7_mOVv9Bo9GkJN9gHcObsoTbYu7YtQJcjmrfu3MIeGdPvVd26Boc6FzkrxQ-ElM7Bg8ROOlrFA=w338-h436-no",
+    "alt": "Key signature with one sharp: F sharp."
+  },
+  {
+    "_id": {
+      "$oid": ""
+    },
+    "answers": [
+      {
+        "content": "A# major",
+        "type": "incorrect"
+      },
+      {
+        "content": "D major",
+        "type": "incorrect"
+      },
+      {
+        "content": "E major",
+        "type": "incorrect"
+      },
+      {
+        "content": "A major",
+        "type": "correct"
+      },
+    ],
+    "key_signature": "A major",
+    "kitten_value": "160",
+    "accidentals": [
+      "F sharp",
+      "C sharp",
+      "G sharp"
+    ],
+    "answer_explanation": "You can determine the key signature of a major key with sharps by going a half step up from the last sharp. A half step up from G sharp is A, which makes this A major.",
+    "explanation_url": "",
+    "question": "",
+    "alt": "Key signature with three sharps: F sharp, C sharp, and G sharp."
+  },
+  {
+    "_id": {
+      "$oid": ""
+    },
+    "answers": [
+      {
+        "content": "A major",
+        "type": "incorrect"
+      },
+      {
+        "content": "G major",
+        "type": "incorrect"
+      },
+      {
+        "content": "F major",
+        "type": "incorrect"
+      },
+      {
+        "content": "E major",
+        "type": "correct"
+      },
+    ],
+    "key_signature": "E major",
+    "kitten_value": "180",
+    "accidentals": [
+      "F sharp",
+      "C sharp",
+      "G sharp", 
+      "D sharp"
+    ],
+    "answer_explanation": "You can determine the key signature of a major key with sharps by going a half step up from the last sharp. A half step up from D sharp is E, which makes this E major.",
+    "explanation_url": "",
+    "question": "",
+    "alt": "Key signature with four sharps: F sharp, C sharp, G sharp, and D sharp."
+  },
+  {
+    "_id": {
+      "$oid": ""
+    },
+    "answers": [
+      {
+        "content": "D major",
+        "type": "incorrect"
+      },
+      {
+        "content": "F sharp major",
+        "type": "incorrect"
+      },
+      {
+        "content": "A major",
+        "type": "incorrect"
+      },
+      {
+        "content": "B major",
+        "type": "correct"
+      },
+    ],
+    "key_signature": "B major",
+    "kitten_value": "200",
+    "accidentals": [
+      "F sharp",
+      "C sharp",
+      "G sharp", 
+      "D sharp",
+      "A sharp"
+    ],
+    "answer_explanation": "You can determine the key signature of a major key with sharps by going a half step up from the last sharp. A half step up from A sharp is B, which makes this B major.",
+    "explanation_url": "",
+    "question": "",
+    "alt": "Key signature with five sharps: F sharp, C sharp, G sharp, D sharp, and A sharp."
+  },
+  {
+    "_id": {
+      "$oid": ""
+    },
+    "answers": [
+      {
+        "content": "A sharp major",
+        "type": "incorrect"
+      },
+      {
+        "content": "C sharp major",
+        "type": "incorrect"
+      },
+      {
+        "content": "A major",
+        "type": "incorrect"
+      },
+      {
+        "content": "F sharp major",
+        "type": "correct"
+      },
+    ],
+    "key_signature": "F sharp major",
+    "kitten_value": "220",
+    "accidentals": [
+      "F sharp",
+      "C sharp",
+      "G sharp", 
+      "D sharp",
+      "A sharp", 
+      "E sharp"
+    ],
+    "answer_explanation": "You can determine the key signature of a major key with sharps by going a half step up from the last sharp. A half step up from E sharp (also known as F) is F sharp, which makes this F sharp major.",
+    "explanation_url": "",
+    "question": "",
+    "alt": "Key signature with six sharps: F sharp, C sharp, G sharp, D sharp, A sharp, and E sharp"
+  },
+  {
+    "_id": {
+      "$oid": ""
+    },
+    "answers": [
+      {
+        "content": "E major",
+        "type": "incorrect"
+      },
+      {
+        "content": "C major",
+        "type": "incorrect"
+      },
+      {
+        "content": "D sharp major",
+        "type": "incorrect"
+      },
+      {
+        "content": "C sharp major",
+        "type": "correct"
+      },
+    ],
+    "key_signature": "C sharp major",
+    "kitten_value": "220",
+    "accidentals": [
+      "F sharp",
+      "C sharp",
+      "G sharp", 
+      "D sharp",
+      "A sharp", 
+      "E sharp", 
+      "B sharp"
+    ],
+    "answer_explanation": "You can determine the key signature of a major key with sharps by going a half step up from the last sharp. A half step up from B sharp (also known as C) is C sharp, which makes this C sharp major.",
+    "explanation_url": "",
+    "question": "",
+    "alt": "Key signature with seven sharps: F sharp, C sharp, G sharp, D sharp, A sharp, E sharp, and B sharp"
+  },
+  {
+    "_id": {
+      "$oid": ""
+    },
+    "answers": [
+      {
+        "content": "C major",
+        "type": "incorrect"
+      },
+      {
+        "content": "B flat major",
+        "type": "incorrect"
+      },
+      {
+        "content": "D major",
+        "type": "incorrect"
+      },
+      {
+        "content": "F major",
+        "type": "correct"
+      },
+    ],
+    "key_signature": "F major",
+    "kitten_value": "120",
+    "accidentals": [
+      "B flat"
+    ],
+    "answer_explanation": "Sorry, no shortcuts here. You'll just have to remember that the key signature with one flat is F major.",
+    "explanation_url": "",
+    "question": "",
+    "alt": "Key signature with one flat: B flat"
+},
+{
+  "_id": {
+    "$oid": ""
+  },
+  "answers": [
+    {
+      "content": "E flat major",
+      "type": "incorrect"
+    },
+    {
+      "content": "B major",
+      "type": "incorrect"
+    },
+    {
+      "content": "D flat major",
+      "type": "incorrect"
+    },
+    {
+      "content": "B flat major",
+      "type": "correct"
+    },
+  ],
+  "key_signature": "B flat major",
+  "kitten_value": "140",
+  "accidentals": [
+    "B flat", 
+    "E flat"
+  ],
+  "answer_explanation": "You can determine the key signature of a major key with two or more flats by looking at the second-to-last flat. The second-to-last flat here is B flat, so this is B flat major.",
+  "explanation_url": "",
+  "question": "",
+  "alt": "Key signature with two flats: B flat and E flat."
+},
+{
+  "_id": {
+    "$oid": ""
+  },
+  "answers": [
+    {
+      "content": "A flat major",
+      "type": "incorrect"
+    },
+    {
+      "content": "E major",
+      "type": "incorrect"
+    },
+    {
+      "content": "B flat major",
+      "type": "incorrect"
+    },
+    {
+      "content": "E flat major",
+      "type": "correct"
+    },
+  ],
+  "key_signature": "E flat major",
+  "kitten_value": "160",
+  "accidentals": [
+    "B flat", 
+    "E flat", 
+    "A flat"
+  ],
+  "answer_explanation": "You can determine the key signature of major keys with flats by looking at the second-to-last flat. The second-to-last flat here is E flat, so this is E flat major.",
+  "explanation_url": "",
+  "question": "",
+  "alt": "Key signature with three flats: B flat, E flat, and A flat."
+},
+{
+  "_id": {
+    "$oid": ""
+  },
+  "answers": [
+    {
+      "content": "A major",
+      "type": "incorrect"
+    },
+    {
+      "content": "D major",
+      "type": "incorrect"
+    },
+    {
+      "content": "G flat major",
+      "type": "incorrect"
+    },
+    {
+      "content": "A flat major",
+      "type": "correct"
+    },
+  ],
+  "key_signature": "A flat major",
+  "kitten_value": "180",
+  "accidentals": [
+    "B flat", 
+    "E flat", 
+    "A flat", 
+    "D flat"
+  ],
+  "answer_explanation": "You can determine the key signature of major keys with flats by looking at the second-to-last flat. The second-to-last flat here is A flat, so this is A flat major.",
+  "explanation_url": "",
+  "question": "",
+  "alt": "Key signature with four flats: B flat, E flat, A flat, and D flat."
+},
+{
+  "_id": {
+    "$oid": ""
+  },
+  "answers": [
+    {
+      "content": "D major",
+      "type": "incorrect"
+    },
+    {
+      "content": "E flat major",
+      "type": "incorrect"
+    },
+    {
+      "content": "G flat major",
+      "type": "incorrect"
+    },
+    {
+      "content": "D flat major",
+      "type": "correct"
+    },
+  ],
+  "key_signature": "D flat major",
+  "kitten_value": "200",
+  "accidentals": [
+    "B flat", 
+    "E flat", 
+    "A flat", 
+    "D flat", 
+    "G flat"
+  ],
+  "answer_explanation": "You can determine the key signature of major keys with flats by looking at the second-to-last flat. The second-to-last flat here is D flat, so this is D flat major.",
+  "explanation_url": "",
+  "question": "",
+  "alt": "Key signature with five flats: B flat, E flat, A flat, D flat, and G flat."
+},
+{
+  "_id": {
+    "$oid": ""
+  },
+  "answers": [
+    {
+      "content": "C major",
+      "type": "incorrect"
+    },
+    {
+      "content": "G major",
+      "type": "incorrect"
+    },
+    {
+      "content": "D flat major",
+      "type": "incorrect"
+    },
+    {
+      "content": "G flat major",
+      "type": "correct"
+    },
+  ],
+  "key_signature": "G flat major",
+  "kitten_value": "220",
+  "accidentals": [
+    "B flat", 
+    "E flat", 
+    "A flat", 
+    "D flat", 
+    "G flat",
+    "C flat"
+  ],
+  "answer_explanation": "You can determine the key signature of major keys with flats by looking at the second-to-last flat. The second-to-last flat here is G flat, so this is G flat major.",
+  "explanation_url": "",
+  "question": "",
+  "alt": "Key signature with six flats: B flat, E flat, A flat, D flat, G flat, and C flat."
+},
+{
+  "_id": {
+    "$oid": ""
+  },
+  "answers": [
+    {
+      "content": "C major",
+      "type": "incorrect"
+    },
+    {
+      "content": "F major",
+      "type": "incorrect"
+    },
+    {
+      "content": "F flat major",
+      "type": "incorrect"
+    },
+    {
+      "content": "C flat major",
+      "type": "correct"
+    },
+  ],
+  "key_signature": "C flat major",
+  "kitten_value": "240",
+  "accidentals": [
+    "B flat", 
+    "E flat", 
+    "A flat", 
+    "D flat", 
+    "G flat",
+    "C flat", 
+    "F flat"
+  ],
+  "answer_explanation": "You can determine the key signature of major keys with flats by looking at the second-to-last flat. The second-to-last flat here is C flat, so this is C flat major.",
+  "explanation_url": "",
+  "question": "",
+  "alt": "Key signature with seven flats: B flat, E flat, A flat, D flat, G flat, C flat, and F flat."
+},
+
+]
+
+export default quizData;
