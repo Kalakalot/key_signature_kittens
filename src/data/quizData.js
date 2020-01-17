2807fead9e265719bfdcd067e82e59311215edc5
Julia A Kingrey <juliakingreyworks@gmail.com>
2020-01-17 12:55:53 -0800

    Moved all images to assets and imported them in test database.

--- src/data/quizData.js
+++ src/data/quizData.js
@@ -1,3 +1,10 @@
+import C from '../assets/key_sig_images/C.png';
+import C_explanation from '../assets/explanation_images/C_explanation.png';
+import D from '../assets/key_sig_images/D.png';
+import D_explanation from '../assets/explanation_images/D_explanation.png';
+import G from '../assets/key_sig_images/G.png';
+import G_explanation from '../assets/explanation_images/G_explanation.png';
+
 const quizData = 
 [
   {
@@ -28,9 +35,10 @@ const quizData =
       ""
     ],
     "answer_explanation": "C major is the only major key with no accidentals. You \"C\" no sharps or flats here!",
-    "explanation_url": "https://placebear.com/300/300",
-    "question": "https://lh3.googleusercontent.com/bwDmcgD4YDYzKJstp1qYHFDWsqtR4K2SN9clK5B-Mc7v3zM6oXvAviCA7AODILxfwgQ9be2z6ZY9nwetu0SteBSyrBN44PXE6PcVyiLI4Jtl9wXMbJJjhNApjCYO_fPbcu7ElPJWv-ca1fcOnmH6sdjjlPklb1OhYPQdTEEmQWJtjbUXnJip0hmHs0rTEUw_Hwe7TY15Evz5SzAebSlmioLuBICEG_btcpWIT-_hYKy_TyoMfTiMCmxJi8yT5td-WIb_Bwx-0LIymGUMoJXKsbNm6g4Tt40g9hejmso4bS0NOVPaWtF4DF5evXIBbWUllrlhIhkEg0eRplVgMdtsfUi4NLeLSuPrk9tznHBILaFMA5MB8CJDsTKtKqNBhWCQ1Fg8N0ggw3yxsNP5K5ujyMOgnN2F0j4YAn9QdmdZ0ZKFWVL2j0J2P6bexg0JWciE9uDnwer5BcmiUqvtH0Q6htSB3oNx_ZDDN0gGjY7zrOm5fCkWQZKANq5v6Q9DBL9u1ZxdWQbvwI3bGd60pn6spgsjryX4ejAGKXtVwoOaCGvGbxqmrUnfZFBi_GxZCMZI2FGUcZ4Ntiz6p0jJrRhSUdxndB31dTKIMI2lriX78Wf9GVG5fhbOrilNMKw5I2PkpeahJjPDqpY6ZJuLEtGXlnFH6w8wMkYqBcrAQVUXZZ_X-Y-QQOwT0w=w298-h436-no",
-    "bass_url": "",
+    "explanation_url": C_explanation,
+    // "explanation_url": "https://placebear.com/300/300",
+    "question": C,
+    // "question": "https://lh3.googleusercontent.com/bwDmcgD4YDYzKJstp1qYHFDWsqtR4K2SN9clK5B-Mc7v3zM6oXvAviCA7AODILxfwgQ9be2z6ZY9nwetu0SteBSyrBN44PXE6PcVyiLI4Jtl9wXMbJJjhNApjCYO_fPbcu7ElPJWv-ca1fcOnmH6sdjjlPklb1OhYPQdTEEmQWJtjbUXnJip0hmHs0rTEUw_Hwe7TY15Evz5SzAebSlmioLuBICEG_btcpWIT-_hYKy_TyoMfTiMCmxJi8yT5td-WIb_Bwx-0LIymGUMoJXKsbNm6g4Tt40g9hejmso4bS0NOVPaWtF4DF5evXIBbWUllrlhIhkEg0eRplVgMdtsfUi4NLeLSuPrk9tznHBILaFMA5MB8CJDsTKtKqNBhWCQ1Fg8N0ggw3yxsNP5K5ujyMOgnN2F0j4YAn9QdmdZ0ZKFWVL2j0J2P6bexg0JWciE9uDnwer5BcmiUqvtH0Q6htSB3oNx_ZDDN0gGjY7zrOm5fCkWQZKANq5v6Q9DBL9u1ZxdWQbvwI3bGd60pn6spgsjryX4ejAGKXtVwoOaCGvGbxqmrUnfZFBi_GxZCMZI2FGUcZ4Ntiz6p0jJrRhSUdxndB31dTKIMI2lriX78Wf9GVG5fhbOrilNMKw5I2PkpeahJjPDqpY6ZJuLEtGXlnFH6w8wMkYqBcrAQVUXZZ_X-Y-QQOwT0w=w298-h436-no",
     "alt": "Key signature with no sharps or flats"
   },
   {
@@ -62,9 +70,10 @@ const quizData =
       "C sharp"
     ],
     "answer_explanation": "You can determine the key signature of a major key with sharps by going a half step up from the last sharp. A half step up from C sharp is D, which makes this D major.",
-    "explanation_url": "https://lh3.googleusercontent.com/SofovhF-UzRgVWvxx1jko-sQ39nZkChMmRp4kri5Ds-fRCFyaK8xRJDMMV61lezuAV93r69V1K8MfN3LqKNJhVHBhRc2QI-TyVN1Ryyx7-q7OaTsTJDMth-V_YhwYnL6sT5QRlpRtafc3212-h44B7pkmNzkZa0mOCk1C1vsBUaxzCate0wi2E3vSR9XGEWWoFFfGhaZrHStKK8wF-COCoCBZk9NIuiUkV5KLse_vZlDTq_JZXCyX9kgOH8-zMK_FmZVlqR7SoPDezcqCDK8UiZERGqxc2o0seWTAasXUg33S6F6S3EnHJBSkReeDTK0T13mCzKSpDdztCEdY3E43yGDt7T-z7tQiEJqnzKPGhvcxHYxraWjbss7CZtyQoV4dsCkHtDq2YmTLSGSh26qOWQuJ2VwNfutkAOFJvz5sBT1bjI8SFy7GWXM8SwX4RaVObCNd51MD2ZiKoRhI-Tmti9msA1QGNSkq9mVncH5Aly4yTmPWzw5lgboYktAk-t5INLpX55QctqKaLNI_NXx4Fpw0Gj6pBhVpkZ57cekFAUWk3fx8zT_B_wDKbAUiE936QSEYAF_LQQkG1tyyUtHzvn72MDFAigg2yOxb6vPyocnwb6mxHTC9dR0yhzFk6yMd6ulu3O7QrpRIQQl290c9nZ98_sNhZo9pBgYs-RHTCuWlP39NWlTGA=w394-h436-no",
-    "question": "https://lh3.googleusercontent.com/2r90D7dnV-SvpyySjKTSzZsstTcaoWtE61wreACjnWkHT6kXfV3p3qsd-NqNzV-VwXSoD8z2MKn_zoeyHAgmfVNL3mc2rQI8y0IVHNmzucDUPpvYL74vArZG5iSj3Iil20oGLOnKk-ydPdrF9wIoNgVdLLK0ixhfJlYcYl_ymIKzzpz3PZPMSWaOakcl03BuSSPqs34w9Kkjn_HiKNMOB0kMs_SEUTT14S_lwo8QXf2TBuuUG6VCA0s7sUjxxolaTooY9bWYw70PLWH2canm_pXoHbY-zAuiN7MZfJrKA9Nwua_FW2yyif9csrLwZ8DCraxPxyDDQTucGWtIjJFIAmpaXrr-DmJSUdr5oPkNw07Fd0Ef9O4RDWlQKzuHZjhBm2O2Fw3-q3zTJbp2uwygbgdbYzYAMjYmpczP3jFx75gi_z3qx7npbGsnB7xxD53qJKshwsMopo1-tPwNfaw8dtxnTUFMZ6X-lnP7uibJ_ZTUcmZerzvVimkZKhNRjeP9NwN8g1IbKLXXyjBUZqnxGX-LmZ8jZQJPVt4kNXcQNIBZxRfsLLAC_NCKZy9NKYaxvjl3TrvsmuLo7UsxwA-gtWRymy8M3ZAC9r5H9Kj1v-VNtyG_CIvJ88ZLeHhmlEung3FlHRSaRbDbqFMICU3rYh_rewStjaRpI_DzbV35-pZ2BLeIUcnArg=w394-h436-no",
-    "bass_url": "",
+    "explanation_url": D_explanation, 
+    // "explanation_url": "https://lh3.googleusercontent.com/SofovhF-UzRgVWvxx1jko-sQ39nZkChMmRp4kri5Ds-fRCFyaK8xRJDMMV61lezuAV93r69V1K8MfN3LqKNJhVHBhRc2QI-TyVN1Ryyx7-q7OaTsTJDMth-V_YhwYnL6sT5QRlpRtafc3212-h44B7pkmNzkZa0mOCk1C1vsBUaxzCate0wi2E3vSR9XGEWWoFFfGhaZrHStKK8wF-COCoCBZk9NIuiUkV5KLse_vZlDTq_JZXCyX9kgOH8-zMK_FmZVlqR7SoPDezcqCDK8UiZERGqxc2o0seWTAasXUg33S6F6S3EnHJBSkReeDTK0T13mCzKSpDdztCEdY3E43yGDt7T-z7tQiEJqnzKPGhvcxHYxraWjbss7CZtyQoV4dsCkHtDq2YmTLSGSh26qOWQuJ2VwNfutkAOFJvz5sBT1bjI8SFy7GWXM8SwX4RaVObCNd51MD2ZiKoRhI-Tmti9msA1QGNSkq9mVncH5Aly4yTmPWzw5lgboYktAk-t5INLpX55QctqKaLNI_NXx4Fpw0Gj6pBhVpkZ57cekFAUWk3fx8zT_B_wDKbAUiE936QSEYAF_LQQkG1tyyUtHzvn72MDFAigg2yOxb6vPyocnwb6mxHTC9dR0yhzFk6yMd6ulu3O7QrpRIQQl290c9nZ98_sNhZo9pBgYs-RHTCuWlP39NWlTGA=w394-h436-no",
+    "question": D,
+    // "question": "https://lh3.googleusercontent.com/2r90D7dnV-SvpyySjKTSzZsstTcaoWtE61wreACjnWkHT6kXfV3p3qsd-NqNzV-VwXSoD8z2MKn_zoeyHAgmfVNL3mc2rQI8y0IVHNmzucDUPpvYL74vArZG5iSj3Iil20oGLOnKk-ydPdrF9wIoNgVdLLK0ixhfJlYcYl_ymIKzzpz3PZPMSWaOakcl03BuSSPqs34w9Kkjn_HiKNMOB0kMs_SEUTT14S_lwo8QXf2TBuuUG6VCA0s7sUjxxolaTooY9bWYw70PLWH2canm_pXoHbY-zAuiN7MZfJrKA9Nwua_FW2yyif9csrLwZ8DCraxPxyDDQTucGWtIjJFIAmpaXrr-DmJSUdr5oPkNw07Fd0Ef9O4RDWlQKzuHZjhBm2O2Fw3-q3zTJbp2uwygbgdbYzYAMjYmpczP3jFx75gi_z3qx7npbGsnB7xxD53qJKshwsMopo1-tPwNfaw8dtxnTUFMZ6X-lnP7uibJ_ZTUcmZerzvVimkZKhNRjeP9NwN8g1IbKLXXyjBUZqnxGX-LmZ8jZQJPVt4kNXcQNIBZxRfsLLAC_NCKZy9NKYaxvjl3TrvsmuLo7UsxwA-gtWRymy8M3ZAC9r5H9Kj1v-VNtyG_CIvJ88ZLeHhmlEung3FlHRSaRbDbqFMICU3rYh_rewStjaRpI_DzbV35-pZ2BLeIUcnArg=w394-h436-no",
     "alt": "Key signature with two sharps: F sharp and C sharp"
   },
   {
@@ -93,9 +102,10 @@ const quizData =
     "kitten_value": "120",
     "accidentals": "F sharp",
     "answer_explanation": "You can determine the key signature of a major key with sharps by going a half step up from the last sharp. A half step up from F sharp is G, which makes this G major.",
-    "explanation_url": "https://placebear.com/300/300",
-    "question": "https://lh3.googleusercontent.com/B6KCZzCsuM9TsSmTP_CCrv3TdHHqCzaDtHDff_fZRLfyN_rh_b9Pb6AlVL8wguf9Bputks0w9JB41k3K0Q-eGwWg5aWz-dXnJaCVQYoPfUXmxAh5qdRvGKF6t380jM9PEo62J5JoowpnskAyZv0WCV8Sr1j3ZBsmo2Kgqw9ix3iW_Q8ruYEvtW50lggNYtBAjuxZ8_zVlgsQj9mwMbIdI8rkV0iSyVNBklTT33DieiWKVMX5GKZxjevMCTCzm8zpgwFgjKhmaWmW7BT0vNaAvVSolvCP-LV0RNOdLBesvuzi-wYHEioLpJU2Tc3Lraz6D0jq8vykJIDNtKpSHrKGeEaDglWW40IzQfXJfDkMsQfYqE6j73Fy5uhneKthZX3L0YPUBHyYD0srNLn9wSioUieI_JuftVdNjdO9NF-gur4Lv0z9pFX84IddSzbTX54PTq0HPql9IuWz7QL-tP_gzKrX1TJIyA02bytHC5Rv2Df6up3qUqy4dJ3M5DT19aOB41I26kIjeD5Z1HGa5mLybsIvb3NvKi3qwSx917rlo-hN7SMqo8GjISWwUACw7xCIB9ojlqrvFVoZJs15u-XbUb4yHWDININZS_ryPtH8ZmFo7_mOVv9Bo9GkJN9gHcObsoTbYu7YtQJcjmrfu3MIeGdPvVd26Boc6FzkrxQ-ElM7Bg8ROOlrFA=w338-h436-no",
-    "bass_url": "",
+    "explanation_url": G_explanation,
+    // "explanation_url": "https://placebear.com/300/300",
+    "question": G,
+    // "question": "https://lh3.googleusercontent.com/B6KCZzCsuM9TsSmTP_CCrv3TdHHqCzaDtHDff_fZRLfyN_rh_b9Pb6AlVL8wguf9Bputks0w9JB41k3K0Q-eGwWg5aWz-dXnJaCVQYoPfUXmxAh5qdRvGKF6t380jM9PEo62J5JoowpnskAyZv0WCV8Sr1j3ZBsmo2Kgqw9ix3iW_Q8ruYEvtW50lggNYtBAjuxZ8_zVlgsQj9mwMbIdI8rkV0iSyVNBklTT33DieiWKVMX5GKZxjevMCTCzm8zpgwFgjKhmaWmW7BT0vNaAvVSolvCP-LV0RNOdLBesvuzi-wYHEioLpJU2Tc3Lraz6D0jq8vykJIDNtKpSHrKGeEaDglWW40IzQfXJfDkMsQfYqE6j73Fy5uhneKthZX3L0YPUBHyYD0srNLn9wSioUieI_JuftVdNjdO9NF-gur4Lv0z9pFX84IddSzbTX54PTq0HPql9IuWz7QL-tP_gzKrX1TJIyA02bytHC5Rv2Df6up3qUqy4dJ3M5DT19aOB41I26kIjeD5Z1HGa5mLybsIvb3NvKi3qwSx917rlo-hN7SMqo8GjISWwUACw7xCIB9ojlqrvFVoZJs15u-XbUb4yHWDININZS_ryPtH8ZmFo7_mOVv9Bo9GkJN9gHcObsoTbYu7YtQJcjmrfu3MIeGdPvVd26Boc6FzkrxQ-ElM7Bg8ROOlrFA=w338-h436-no",
     "alt": "Key signature with one sharp: F sharp."
   }
 ]
